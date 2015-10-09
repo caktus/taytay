0777e9a17280a484c1848ce7a94bce430ac8bc85
bphillips <bphillips@caktusgroup.com>
2015-10-09 13:24:24 -0400

    #12 - Fixed pep8 issues

--- taytay/views.py
+++ taytay/views.py
@@ -5,7 +5,7 @@ import markovify
 
 def make_markov_chain(album):
     lyrics = ""
-    if album != None:
+    if album is not None:
         qs = Song.objects.filter(album__title=album)
     else:
         qs = Song.objects.all()
@@ -15,12 +15,13 @@ def make_markov_chain(album):
     lyrics_generator = markovify.text.NewlineText(lyrics, state_size=2)
     return lyrics_generator
 
+
 def make_stanza(lyrics_generator):
     stanza = ""
     for _ in range(4):
         while True:
             line = lyrics_generator.make_sentence()
-            if line != None:
+            if line is not None:
                 stanza += (line + "\n")
                 break
     return stanza
@@ -30,18 +31,20 @@ def make_song(album=None):
     lyrics_generator = make_markov_chain(album)
     chorus = make_stanza(lyrics_generator)
     song = (make_stanza(lyrics_generator) + "\n \n" + chorus + "\n \n" +
-     make_stanza(lyrics_generator) + "\n \n" + chorus +
-     "\n \n" + make_stanza(lyrics_generator))
+            make_stanza(lyrics_generator) + "\n \n" + chorus +
+            "\n \n" + make_stanza(lyrics_generator))
     return song
+
 
 def make_title(song):
     while True:
         title_generator = markovify.text.NewlineText(song)
         title = title_generator.make_sentence()
-        if title != None:
+        if title is not None:
             title_list = title.split(" ")
             title = " ".join(title_list[0:3])
             return title
+
 
 def song_generator(request):
     """Generate a new song."""
