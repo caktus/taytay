8d7c5812b5da41d66a447230b15e703b0d90d5c5
Mark Lavin <mlavin@caktusgroup.com>
2015-10-09 08:41:21 -0400

    Wrong parens.

--- taytay/wsgi.py
+++ taytay/wsgi.py
@@ -15,7 +15,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
-dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__), '.env')))
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taytay.settings')
 
 application = DjangoWhiteNoise(get_wsgi_application())
