696361f530f339c0cf96dcad4539186d78f3e0d5
Mark Lavin <mlavin@caktusgroup.com>
2015-10-08 20:39:00 -0400

    Working through fetching all of the album and song lyrics.

--- taytay/management/commands/fetch_song_data.py
+++ taytay/management/commands/fetch_song_data.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
+import requests
+
+from ... import models
+
 
 class Command(BaseCommand):
     help = 'Fetch song data from baelor.io'
@@ -9,3 +13,37 @@ class Command(BaseCommand):
         api_key = getattr(settings, 'BAELOR_API_KEY', None)
         if not api_key:
             raise CommandError('BAELOR_API_KEY has not been configured.')
+        auth = {'Authorization': api_key}
+        # Get albums
+        response = requests.get('https://baelor.io/api/v0/albums', headers=auth).json()
+        if response['error']:
+            raise CommandError('Error fetching albums: {error}'.format(**response))
+        else:
+            for item in response['result']:
+                slug = item['slug']
+                additional = {
+                    'title': item['name'],
+                    'label': item['label'],
+                    'genres': item['genres'],
+                    'producers': item['producers'],
+                }
+                album, _ = models.Album.objects.update_or_create(
+                    slug=slug, defaults=additional)
+                for song in item['songs']:
+                    if song['has_lyrics']:
+                        lyrics = requests.get(
+                            'https://baelor.io/api/v0/songs/{slug}/lyrics'.format(**song),
+                            headers=auth).json()
+                        if lyrics['error']:
+                            self.stderr.write(
+                                'Error fetching song "{title}": {error}\n'.format(**song))
+                        else:
+                            rest = {
+                                'title': song['title'],
+                                'album': album,
+                                'lyrics': lyrics['result']['lyrics'],
+                                'writers': song['writers'],
+                                'producers': song['producers'],
+                            }
+                            models.Song.objects.update_or_create(
+                                slug=song['slug'], defaults=rest)
--- taytay/tests/test_commands.py
+++ taytay/tests/test_commands.py
@@ -1,8 +1,11 @@
 import io
+from unittest.mock import patch, Mock
 
 from django.core.management import call_command
 from django.core.management.base import CommandError
-from django.test import TestCase
+from django.test import override_settings, TestCase
+
+from .. import models
 
 
 class CommandMixin(object):
@@ -19,13 +22,55 @@ class CommandMixin(object):
         return stdout, stderr
 
 
+@patch('requests.get')
+@override_settings(BAELOR_API_KEY='XXXXXX')
 class FetchSongDataTestCase(CommandMixin, TestCase):
     """Populate song data from Baelor API."""
 
     command = 'fetch_song_data'
 
-    def test_not_configured(self):
+    def test_not_configured(self, mock_get):
         """Raise error if the API key is not configured."""
         with self.settings(BAELOR_API_KEY=None):
             with self.assertRaises(CommandError):
                 self.call_command()
+        self.assertFalse(mock_get.called)
+
+    def test_fetch_albums(self, mock_get):
+        """Fetch all albums."""
+        album_response = Mock()
+        album_response.json.return_value = {
+            "result": [
+                {
+                    "slug": "taylor-swift",
+                    "name": "Taylor Swift",
+                    "released_at": "2006-10-24T00:00:00",
+                    "length": "00:38:26",
+                    "label": "Big Machine",
+                    "genres": [
+                        "Country"
+                    ],
+                    "producers": [
+                        "Scott Borchetta",
+                        "Nathan Chapman",
+                        "Robert Ellis Orrall"
+                    ],
+                    "songs": [
+                    ],
+                    "album_cover": {
+                        "image_id": "c780c63f-8f19-4961-2873-08d21467e516"
+                    }
+                },
+            ],
+            "error": None,
+            "success": True
+        }
+        mock_get.side_effect = [
+            album_response,
+        ]
+        self.call_command()
+        mock_get.assert_called_with(
+            'https://baelor.io/api/v0/albums', headers={'Authorization': 'XXXXXX'})
+        albums = models.Album.objects.all()
+        self.assertEqual(albums.count(), 1)
+        self.assertEqual(albums[0].title, 'Taylor Swift')
