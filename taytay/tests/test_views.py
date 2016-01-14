d4bfc9fba84525eae18419de7f9ed377fceb3ab8
Mark Lavin <mlavin@caktusgroup.com>
2016-01-14 15:47:50 -0500

    Fixing mock for detail view.

--- taytay/tests/test_views.py
+++ taytay/tests/test_views.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import reverse
 from django.test import override_settings, TestCase
 from unittest.mock import patch
 
+
 @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
 class SongGeneratorTestCase(TestCase):
     """Page to generate lyrics."""
@@ -10,6 +11,8 @@ class SongGeneratorTestCase(TestCase):
     @patch("taytay.views.make_title")
     def test_render_page(self, mock_title, mock_song):
         """Grab the page."""
+        mock_song.return_value = 'Shake it off...'
+        mock_title.return_value = 'Shake It Off'
         with self.assertTemplateUsed('taytay/song-generator.html'):
             response = self.client.get(reverse('new-song'))
             self.assertEqual(response.status_code, 200)
